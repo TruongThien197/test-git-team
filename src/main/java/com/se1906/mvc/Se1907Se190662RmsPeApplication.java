e6a945f79611bd008779bfeb82e252f2dd41cea5
Nguyễn Thiên Trường <xayda.ln@gmail.com>
2026-05-17 10:48:17 +0700

    Initial Project

---
+++ src/main/java/com/se1906/mvc/Se1907Se190662RmsPeApplication.java
@@ -0,0 +1,13 @@
+package com.se1906.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se1907Se190662RmsPeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se1907Se190662RmsPeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se1906/mvc/Se1907Se190662RmsPeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se1906.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se1907Se190662RmsPeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
